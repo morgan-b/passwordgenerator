229811b9ac3a36166272befed60f64ae42dca87b
Morgan Bailey <mabailey079@Gmail.com>
2021-01-08 16:23:02 -0500

    nested the input function within the passwordGenerator function and added a listener event to listen for a user click

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -9,53 +9,89 @@ var onlyLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var onlySpecial = ['#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '['];
 var onlyNumeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-//create function for user inputs for password
-function generateInputs() {
-
-    //prompt and create variable for user input for character length
-
-    var length = parseInt(prompt("How many characters would you like your password to contain? Choose between 8 and 128"));
-
-    //alerts user if they have entered a number less than 8 or more than 128
-    if (length < 8) {
-        alert('The password must be at least 8 characters!');
-        return;
-    }
-    if (length > 128) {
-        alert('The password must be less than 128 characters!');
-        return;
-    }
-
-    //prompt user for special characters
-    var shouldIncludeSpecialCharacters = confirm("Do you want to include special characters?");
-
-    //prompt user for numeric characters
-    var shouldIncludeNumeric = confirm("Do you want to include numeric characters?");
-
-    //prompt user for lowercase characters
-    var shouldIncludeLowercase = confirm("Do you want to include lowercase characters?");
-
-    //prompt user for uppercase characters
-    var shouldIncludeUppercase = confirm("Do you want to include uppercase characters?");
-
-    //alert users if they did not select one of the above for their password
-    if (!shouldIncludeLowercase && !shouldIncludeUppercase && !shouldIncludeNumeric && !shouldIncludeSpecialCharacters) {
-        alert("Your password must contain at least one special, numeric, lowercase, or uppercase character");
-        return;
-    }
-
-    var questionOptions = {
-        length: length,
-        specialCharacters: shouldIncludeSpecialCharacters,
-        numeric: shouldIncludeNumeric,
-        lowerCase: shouldIncludeLowercase,
-        upperCase: shouldIncludeUppercase
-    };
-
-    return questionOptions;
-
-}
+//create function for user inputs for password and to generate password
 
 function generatePassword() {
 
- 
+
+    //this function generates the prompts and user values for password
+    function generateInputs() {
+
+        //prompt and create variable for user input for character length
+
+        var length = parseInt(prompt("How many characters would you like your password to contain? Choose between 8 and 128"));
+
+        //alerts user if they have entered a number less than 8 or more than 128
+        if (length < 8) {
+            alert('The password must be at least 8 characters!');
+            return;
+        }
+        if (length > 128) {
+            alert('The password must be less than 128 characters!');
+            return;
+        }
+
+        //prompt user for special characters
+        var shouldIncludeSpecialCharacters = confirm("Do you want to include special characters?");
+
+        //prompt user for numeric characters
+        var shouldIncludeNumeric = confirm("Do you want to include numeric characters?");
+
+        //prompt user for lowercase characters
+        var shouldIncludeLowercase = confirm("Do you want to include lowercase characters?");
+
+        //prompt user for uppercase characters
+        var shouldIncludeUppercase = confirm("Do you want to include uppercase characters?");
+
+        //alert users if they did not select one of the above for their password
+        if (!shouldIncludeLowercase && !shouldIncludeUppercase && !shouldIncludeNumeric && !shouldIncludeSpecialCharacters) {
+            alert("Your password must contain at least one special, numeric, lowercase, or uppercase character");
+            return;
+        }
+
+
+        var questionOptions = {
+            length: length,
+            specialCharacters: shouldIncludeSpecialCharacters,
+            numeric: shouldIncludeNumeric,
+            lowerCase: shouldIncludeLowercase,
+            upperCase: shouldIncludeUppercase
+        };
+
+        return questionOptions;
+
+    }
+
+    var options = generateInputs();
+    console.log(options);
+
+    var passwordPool = [];
+    console.log(passwordPool);
+
+    if (options.specialCharacters) {
+        for (i = 0; i < onlySpecial.length; ++i) {
+            passwordPool.push(onlySpecial[i]);
+        }
+    }
+    if (options.numeric) {
+        for (i = 0; i < onlyNumeric.length; ++i) {
+            passwordPool.push(onlyNumeric[i]);
+        }
+    }
+    if (options.lowerCase) {
+        for (i = 0; i < onlyLower.length; ++i) {
+            passwordPool.push(onlyLower[i]);
+        }
+    }
+    if (options.upperCase) {
+        for (i = 0; i < onlyUpper.length; ++i) {
+            passwordPool.push(onlyUpper[i]);
+        }
+    }
+
+    var finalPassword = [];
+
+}
+
+// listens for user to click the button and then starts the generatePassword funtion
+generatePasswordButton.addEventListener('click', generatePassword);
